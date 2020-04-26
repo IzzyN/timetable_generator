847e3b0569c1c235b27b1e42901f11d83dd120a7
Isobel Norris <isobel.norris@gmail.com>
2020-04-26 15:01:39 +1000

    added example csv file

--- timetable_generator.py
+++ timetable_generator.py
@@ -1,7 +1,7 @@
 import csv
 from pprint import pprint
 
-preferences_file_name = 'Term2lessonpreferencesKeyInfo.csv'
+preferences_file_name = 'Term2lessonpreferencesEXAMPLE.csv'
 fieldnames = ["Child Name", 'Monday Start', 'Monday End', 'Tuesday Start', 'Tuesday End', 'Wednesday Start', 'Wednesday End', 'Thursday Start', 'Thursday End']
 
 days = set([]) 	#SET
@@ -66,28 +66,33 @@ def extracting_info_from_file(preferences_file_name):
 				lists_dictionary[list_name] = [converts_nones(row[cell])]
 		
 
+def find_min_and_max_time(day):
+	for time in ['start', 'end']:	
+		lists_dictionary[f'{day}_{time}s_times'] = []
+		lists_dictionary[f'{day}_{time}s_times_no_any'] = []
+		for i in range(len(lists_dictionary[f'{day}_{time}s'])):
+			item = lists_dictionary[f'{day}_{time}s'][i]
+			if item:
+				lists_dictionary[f'{day}_{time}s_times'] += [item]
+				if item != 'Any':
+					item = int(item)
+					lists_dictionary[f'{day}_{time}s_times_no_any'].append(item)
+
+		pprint(lists_dictionary[f'{day}_{time}s_times_no_any'])
+
+	print(f"max {day}_{time}s_times_no_any: {max(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
+	print(f"min {day}_{time}s_times_no_any: {min(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
+
+
 extracting_info_from_file(preferences_file_name)
 
 least_popular_day, days_sorted, days_dict = find_least_popular_day()
 
+for day in days_sorted:
+	find_min_and_max_time(day)
 
-day = least_popular_day
-print(f'DAY {day}')
-for time in ['start', 'end']:	
-	lists_dictionary[f'{day}_{time}s_times'] = []
-	lists_dictionary[f'{day}_{time}s_times_no_any'] = []
-	for i in range(len(lists_dictionary[f'{day}_{time}s'])):
-		item = lists_dictionary[f'{day}_{time}s'][i]
-		if item:
-			lists_dictionary[f'{day}_{time}s_times'] += [item]
-			if item != 'Any':
-				item = int(item)
-				lists_dictionary[f'{day}_{time}s_times_no_any'].append(item)
-
-	pprint(lists_dictionary[f'{day}_{time}s_times_no_any'])
-
-print(f"max {day}_{time}s_times_no_any: {max(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
-print(f"min {day}_{time}s_times_no_any: {min(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
+if lists_dictionary['child_names']:
+	print('eww kids')
 
 #pprint(lists_dictionary)
 
