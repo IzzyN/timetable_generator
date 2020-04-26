d88f714d88a6b27a79d4162d4e62ec7b25f995f5
Isobel Norris <isobel.norris@gmail.com>
2020-04-26 14:51:05 +1000

    converted to using dictionary of lists

--- timetable_generator.py
+++ timetable_generator.py
@@ -1,19 +1,12 @@
 import csv
+from pprint import pprint
 
 preferences_file_name = 'Term2lessonpreferencesKeyInfo.csv'
 fieldnames = ["Child Name", 'Monday Start', 'Monday End', 'Tuesday Start', 'Tuesday End', 'Wednesday Start', 'Wednesday End', 'Thursday Start', 'Thursday End']
 
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday']
+days = set([]) 	#SET
 
-names = []
-monday_starts = []
-monday_ends = []
-tuesday_starts = []
-tuesday_ends = []
-wednesday_starts = []
-wednesday_ends = []
-thursday_starts = []
-thursday_ends = []
+lists_dictionary = {}
 
 def test():
 	
@@ -32,44 +25,70 @@ def converts_nones(item):
 		return None
 
 	else:
+		item = item.replace(':','')
 		return item
 
 def find_least_popular_day():
-	days_dict = {	'Monday'	: monday_starts,
-				'Tuesday'	: tuesday_starts,
-				'Wednesday'	: wednesday_starts,
-				'Thursday'	: thursday_starts	}
+
+	start_days_dict = {}
+
+	for key in lists_dictionary:
+		for day in days:
+			if day in key and 'start' in key and 'child' not in key:
+				start_days_dict[day] = lists_dictionary[key]
+				#print(f'DAY {day} KEY {key}')
+		#print(f'KEY: {key}')
 
 	#print(f'sorted days {sorted(days)}')
 
-	days_sorted = sorted(days, key=lambda day: options_counter(days_dict[day]), reverse=True)
+	days_sorted = sorted(days, key=lambda day: options_counter(start_days_dict[day]), reverse=True)
 
 	#print(days_sorted)
 	least_popular_day = days_sorted[-1]
-	return(least_popular_day, days_sorted, days_dict)
+	return(least_popular_day, days_sorted, start_days_dict)
 
 def extracting_info_from_file(preferences_file_name):
 	csvfile = open(preferences_file_name)
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		name, row_monday_start, row_monday_end, row_tuesday_start, row_tuesday_end, row_wednesday_start, row_wednesday_end, row_thursday_start, row_thursday_end = [row[x] for x in fieldnames]
-		#print(name, row_monday_start, row_monday_end)
-		names.append(row['Child Name'])
+		for cell in row:
+			day = (cell.split()[0]).lower()
+			#pprint(f'day {day}')
+			if 'child' not in day:
+				days.add(day)
+			column_name = cell.lower().replace(' ','_')
 
-		monday_starts.append(converts_nones(row_monday_start))
-		monday_ends.append(converts_nones(row_monday_end))
-		tuesday_starts.append(converts_nones(row_tuesday_start))
-		tuesday_ends.append(converts_nones(row_tuesday_end))
-		wednesday_starts.append(converts_nones(row_wednesday_start))
-		wednesday_ends.append(converts_nones(row_wednesday_end))
-		thursday_starts.append(converts_nones(row_thursday_start))
-		thursday_ends.append(converts_nones(row_thursday_end))
+			if f'{column_name}s' in lists_dictionary:
+				lists_dictionary[f'{column_name}s'] = lists_dictionary[f'{column_name}s'] + [converts_nones(row[cell])]
+
+			else:
+				list_name = f'{column_name}s'
+				lists_dictionary[list_name] = [converts_nones(row[cell])]
+		
 
 extracting_info_from_file(preferences_file_name)
 
 least_popular_day, days_sorted, days_dict = find_least_popular_day()
-print(least_popular_day)
 
 
+day = least_popular_day
+print(f'DAY {day}')
+for time in ['start', 'end']:	
+	lists_dictionary[f'{day}_{time}s_times'] = []
+	lists_dictionary[f'{day}_{time}s_times_no_any'] = []
+	for i in range(len(lists_dictionary[f'{day}_{time}s'])):
+		item = lists_dictionary[f'{day}_{time}s'][i]
+		if item:
+			lists_dictionary[f'{day}_{time}s_times'] += [item]
+			if item != 'Any':
+				item = int(item)
+				lists_dictionary[f'{day}_{time}s_times_no_any'].append(item)
+
+	pprint(lists_dictionary[f'{day}_{time}s_times_no_any'])
+
+print(f"max {day}_{time}s_times_no_any: {max(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
+print(f"min {day}_{time}s_times_no_any: {min(lists_dictionary[f'{day}_{time}s_times_no_any'])}")
+
+#pprint(lists_dictionary)
 
 #print(test())
